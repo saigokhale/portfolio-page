1ab1896fe3358741b5f27e4e25bba700c068f44e
saigokhale <71946733+saigokhale@users.noreply.github.com>
2025-08-05 06:36:29 +0000

    Deploying to gh-pages from @ saigokhale/portfolio-page@7d9633580c92bd5912c5565933cda890e7ef29d4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/portfolio-page/https:/drive.google.com/file/d/18UECEe__7C_S9-Dl_lwJuQKKhkxuiU74/view";
+            window.location.href = "/portfolio-page/https:/drive.google.com/file/d/12Agy87mIl4kKLPSi72ZofFc_Gqf0iUTL/view";
           },
         },{id: "books-the-godfather",
           title: 'The Godfather',
